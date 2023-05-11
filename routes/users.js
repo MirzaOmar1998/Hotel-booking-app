4ca3350c3587f6f6102d02862fb90f2498a20a33
mirzafaruk <mirzafaruk018@gmail.com>
2023-05-12 00:17:21 +0600

    20th commit

--- routes/users.js
+++ routes/users.js
@@ -8,3 +8,16 @@ import {
 import { verifyAdmin, verifyToken, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
+//UPDATE
+router.put("/:id", verifyUser, updateUser);
+
+//DELETE
+router.delete("/:id", verifyUser, deleteUser);
+
+//GET
+router.get("/:id", verifyUser, getUser);
+
+//GET ALL
+router.get("/", verifyAdmin, getUsers);
+
+export default router;
